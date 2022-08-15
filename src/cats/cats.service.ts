d12409f5e354977a7a729b970fadba2fba200a7f
SthefanyLorenay <sthefany.souza@estudante.ifms.edu.br>
2022-08-15 19:20:48 -0400

    aula 09/08/2022

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,34 @@
+import { Controller, Injectable } from '@nestjs/common';
+import { CastService } from 'src/cast/cast.service';
+import { createCatDto } from './dto/cats.dto';
+
+
+
+@Injectable()
+export class CatsService {
+    private readonly cats: createCatDto[] = [];
+
+    create(cat: createCatDto) {
+        // inserir no banco de dados usando repository
+        this.cats.push(cat);
+    }
+
+    findAll(): createCatDto[] {
+        //buscara todos os elementos do banco de dados
+        return this.cats;
+    }
+
+    findOne(id: number) {
+        return 'one cat'; //filter
+    }
+    remove(id: number) {
+        return 'Cat deleted'; //retira da lista e pronto
+    }
+    update(cat: createCatDto) {
+        const cat = this.findOne(createCatDto.id);
+    } //atualizar ele na lista
+
+            
+ }
+
+    
